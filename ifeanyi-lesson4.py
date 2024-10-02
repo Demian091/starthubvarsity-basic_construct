62b5fd0ad4fd9db08dd4db68c09c35eb527fb070
Demian091 <ifeanyiogochukwu0@gmail.com>
2024-10-02 12:21:00 +0100

    first commit

---
+++ ifeanyi-lesson4.py
@@ -0,0 +1,11 @@
+# defining and declearing a function
+def ifeanyiOgochukwu():
+    # defining the body of the function
+    name = 'ifeanyi'
+    height = 13.4
+    age = 13
+    ismale = True
+
+    print (f"my name is {name} and am {height}, {age} year oldand yes {ismale}")
+# calling the function
+ifeanyiOgochukwu()
