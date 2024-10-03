eb243de812949b711b308fb3b861c42c5c7a7bbf
gamerzkin <uzomae145@gamil.com>
2024-10-03 23:32:46 +0100

    second commit

---
+++ lesson 5.py
@@ -0,0 +1,10 @@
+num_1 = 3
+num_2 = 4
+num_3 = 3
+
+adding = num_1 + num_2 + num_3 
+mul = num_1 * num_2 * num_3
+# result for addition = 10
+print (adding )
+# result for multiplication
+print (mul)
